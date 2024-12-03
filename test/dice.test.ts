70d9425053c08c7591ddc435ac0603e9749f3e35
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-03 08:46:45 +0100

    RED: test that dice get faces from contructor

--- test/dice.test.ts
+++ test/dice.test.ts
@@ -5,13 +5,25 @@ describe("Dice", () => {
     expect(Dice).toBeDefined();
   });
 
-  it("should have a number of faces", () => {
-    const dice = new Dice();
-    expect(dice.size()).toBeDefined();
-  });
+  describe("size", () => {
+    it("should have a number of faces", () => {
+      const dice = new Dice();
+      expect(dice.size()).toBeDefined();
+    });
 
-  it("should receive number of faces in constructor", () => {
-    const dice = new Dice(6);
-    expect(dice.size()).toBe(6);
+    it("should receive number of faces in constructor", () => {
+      const dice = new Dice(6);
+      expect(dice.size()).toBe(6);
+    });
+
+    it("should receive number of faces in constructor", () => {
+      const dice = new Dice(8);
+      expect(dice.size()).toBe(8);
+    });
+
+    it("should default to 6 faces", () => {
+      const dice = new Dice();
+      expect(dice.size()).toBe(6);
+    });
   });
 });
