a96d1d9838f8df61ad11e355b6bdbbb618f00fe4
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-03 09:35:13 +0100

    REFACTOR

--- test/dice.test.ts
+++ test/dice.test.ts
@@ -5,38 +5,34 @@ describe("Dice", () => {
     expect(Dice).toBeDefined();
   });
 
-  it("should throw an error if faces are less than 4", () => {
+  it("should throw an error if the number of faces passed to the constructor is less than 4", () => {
     expect(() => new Dice(3)).toThrow();
+  });
+
+  it.each([6, 8])(
+    "should create a dice with the specified number of faces",
+    (input) => {
+      const dice = new Dice(input);
+      const actual = dice.size();
+      const expected = input;
+      expect(actual).toBe(expected);
+    }
+  );
+
+  it("should default to 6 faces if no number of faces is passed", () => {
+    const dice = new Dice();
+    const actual = dice.size();
+    const expected = 6;
+    expect(actual).toBe(expected);
   });
 
   /*
   Testing the size method
   */
   describe("size", () => {
-    it("should have a number of faces", () => {
+    it("should return a value", () => {
       const dice = new Dice();
       expect(dice.size()).toBeDefined();
-    });
-
-    it("should receive number of faces in constructor", () => {
-      const dice = new Dice(6);
-      const actual = dice.size();
-      const expected = 6;
-      expect(actual).toBe(expected);
-    });
-
-    it("should receive number of faces in constructor", () => {
-      const dice = new Dice(8);
-      const actual = dice.size();
-      const expected = 8;
-      expect(actual).toBe(expected);
-    });
-
-    it("should default to 6 faces", () => {
-      const dice = new Dice();
-      const actual = dice.size();
-      const expected = 6;
-      expect(actual).toBe(expected);
     });
   });
 });
