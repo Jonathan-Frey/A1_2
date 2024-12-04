4bca3878cc8928b0bd91f4dc09fa161556225a1f
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-04 10:32:48 +0100

    GREEN

--- src/Dice.ts
+++ src/Dice.ts
@@ -1,7 +1,7 @@
 export class Dice {
   private faces: number;
   constructor(faces: number = 6) {
-    if (faces === 3 || faces === 2) {
+    if (faces === 3 || faces === 2 || faces === 1 || faces === 0) {
       throw new Error();
     }
     this.faces = faces;
