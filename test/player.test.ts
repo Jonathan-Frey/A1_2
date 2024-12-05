011d2c4f2a7e831325d21286ef67ae3a418d3abd
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-05 10:13:43 +0100

    RED

--- test/player.test.ts
+++ test/player.test.ts
@@ -1,7 +1,16 @@
 import { Player } from "./Player";
 
 describe("Player", () => {
+  let mockPlayer: Player;
+
+  beforeEach(() => {
+    mockPlayer = {};
+  });
   it("should be defined", () => {
     expect(Player).toBeDefined();
+  });
+
+  it("should not throw an error if a dice is passed to the constructor", () => {
+    expect(new Player(mockPlayer)).not.toThrow();
   });
 });
