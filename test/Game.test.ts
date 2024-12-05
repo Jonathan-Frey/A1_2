b064220eb87b8aa48fc167546b87ac1a8f0a5c06
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-05 15:48:52 +0100

    RED

---
+++ test/Game.test.ts
@@ -0,0 +1,12 @@
+describe("Game", () => {
+  it("should be defined", () => {
+    expect(Game).toBeDefined();
+  });
+
+  it("should not throw an error if a number of players is passed to the constructor", () => {
+    const playerAmount = 1;
+    expect(() => {
+      new Game(playerAmount);
+    }).not.toThrow();
+  });
+});
