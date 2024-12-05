844ea3db912aeda0f069779e8b9dcec75a296022
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-05 15:39:23 +0100

    GREEN

--- src/Player.ts
+++ src/Player.ts
@@ -1,7 +1,14 @@
 import { Dice } from "./Dice";
 
 export class Player {
+  private _points = 0;
   constructor(dice: Dice) {}
 
-  hit() {}
+  get points() {
+    return this._points;
+  }
+
+  hit() {
+    this._points += 2;
+  }
 }
