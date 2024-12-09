c9be18930908abcfa1a26d3afa98d7b019f34c02
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-09 15:39:53 +0100

    RED

--- src/ConsoleUi.ts
+++ src/ConsoleUi.ts
@@ -1,1 +1,5 @@
-export class ConsoleUi {}
+import { UiInput } from "./UiInput";
+
+export class ConsoleUi {
+  constructor(input: UiInput) {}
+}
--- test/ConsoleUi.test.ts
+++ test/ConsoleUi.test.ts
@@ -1,22 +1,18 @@
 import { ConsoleUi } from "../src/ConsoleUi";
 import { ConsoleUiInput } from "../src/ConsoleUiInput";
 import { stdin } from "node:process";
+import { ConsoleUiOutput } from "../src/ConsoleUiOutput";
 
 describe("ConsoleUi", () => {
   it("should be defined", () => {
     expect(ConsoleUi).toBeDefined();
   });
 
-  it("should not throw an exception when instatiated", () => {
-    expect(() => {
-      new ConsoleUi();
-    }).not.toThrow();
-  });
-
-  it("should take an implementation of UiInput as a costructor parameter", () => {
+  it("should take an implementation of UiInput and UiOutput as a costructor parameters", () => {
     const consoleUiInput = new ConsoleUiInput(stdin);
+    const consoleUiOutput = new ConsoleUiOutput();
     expect(() => {
-      new ConsoleUi(consoleUiInput);
+      new ConsoleUi(consoleUiInput, consoleUiOutput);
     }).not.toThrow();
   });
 });
