bfbd021ac1306b0ed110bc7d666efb500d6acd5a
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-09 13:23:43 +0100

    RED

--- test/ConsoleUiInput.test.ts
+++ test/ConsoleUiInput.test.ts
@@ -4,4 +4,13 @@ describe("ConsoleUiInput()", () => {
   it("should be defined", () => {
     expect(ConsoleUiInput).toBeDefined();
   });
+
+  it("should not thor an exception when instatiated", () => {
+    expect(() => new ConsoleUiInput()).not.toThrow();
+  });
+
+  describe("requestInput()", () => {
+    const sut = new ConsoleUiInput();
+    expect(sut.requestInput).toBeDefined();
+  });
 });
