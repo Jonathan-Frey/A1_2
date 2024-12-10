0f87de860410689f513c6ee2810dd38e875f1ec8
Jonathan-Frey <jf223rf@student.lnu.se>
2024-12-10 09:17:14 +0100

    RED

--- test/ConsoleUi.test.ts
+++ test/ConsoleUi.test.ts
@@ -2,6 +2,7 @@ import { ConsoleUi } from "../src/ConsoleUi";
 import { ConsoleUiInput } from "../src/ConsoleUiInput";
 import { stdin } from "node:process";
 import { ConsoleUiOutput } from "../src/ConsoleUiOutput";
+import { UiOutput } from "../src/UiOutput";
 
 describe("ConsoleUi", () => {
   it("should be defined", () => {
@@ -14,5 +15,21 @@ describe("ConsoleUi", () => {
     expect(() => {
       new ConsoleUi(consoleUiInput, consoleUiOutput);
     }).not.toThrow();
+  });
+
+  describe("showWelcome()", () => {
+    it("should call the display method of the UiOutput with the provided string", () => {
+      const mockUiOutput: UiOutput = {
+        display: jest.fn(),
+      };
+      const consoleUiInput = new ConsoleUiInput(stdin);
+      const consoleUi = new ConsoleUi(consoleUiInput, mockUiOutput);
+
+      const input = "Hello, World!";
+
+      consoleUi.showWelcome(input);
+
+      expect(mockUiOutput.display).toHaveBeenCalledWith(input);
+    });
   });
 });
